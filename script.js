06f072df4b7b717fa35015178b444c7aa3c59704
Plan49 <anthonyplanisek1@gmail.com>
2021-04-12 22:09:21 -0400

    finished page

--- script.js
+++ script.js
@@ -7,31 +7,60 @@ function generatePassword() {
   var number = prompt("How long do you want your password")
   var retVal = ""
   
+
+
+
   if (number < 8 || number > 128) {
-    retVal = "yes"
+    retVal = "Please enter a number greater than 8 and less than 128"
     
   }
 
   if (number >= 8 && number <= 128) {
-    characters = ""
+
     var lowercase = confirm("Do you want the password to contain lowercase")
+    var uppercase = confirm("Do you want the password to contain uppercase")
+    var numbers = confirm("Do you want the password to contain numbers")
+    var specialChar = confirm("Do you want the password to contain special characters")
+
+
     if (lowercase == true) {
-      
+      charactersL = "abcdefghijklmnopqrstuvwxyz" 
+    } else {
+      charactersL = ""
     }
+      if (uppercase == true) {
+        charactersU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+      } else {
+        charactersU = ""
+      }
+        if (numbers == true) {
+          charactersN = "0123456789" 
+        } else {
+          charactersN = ""
+        }
+          if (specialChar == true) {
+            charactersS = "0123456789" 
+          } else {
+            charactersS = ""
+          }
+          
+      
 
 
 
 
 
 
-    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    characters = charactersL + charactersU + charactersN + charactersS
   
     for (var i = 0, n = characters.length; i < number; ++i) {
     retVal += characters.charAt(Math.floor(Math.random() * n));
   }
   
-  }
-  return retVal;
+  
+ 
+}
+return retVal;
 }
 
 
